664975d9140f957f37d9941c9396640cdb869523
wushijiang <wushijiang@huan.tv.com>
2021-07-09 10:25:48 +0800

    [animation]:add:新增动画例子

---
+++ 随手动画例子/cyberpunk/page/index/static/js/index.js
@@ -0,0 +1,1 @@
+import '../css/index.css';//导入css 为了监听修改，后续会替换
---
+++ 随手动画例子/cyberpunk/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const webpack=require('webpack');
+
+module.exports={
+    mode: "development",
+    entry:{
+        index:"./page/index/static/js/index.js",
+    },
+    devServer:{
+        watchContentBase: true,
+        contentBase:"./page",
+        host: "0.0.0.0",
+        hot:true,
+        port:8081,
+        inline: true,//设置外部引入
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify:true,
+            filename:"index.html" ,
+            template:"./page/index/index.html",
+            hash:true,
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module:{
+      rules:[
+          {
+              test:/\.js$/,
+              use:['babel-loader'],
+          },
+          {
+              test: /\.css$/,
+              use: [{ loader :'style-loader'},
+                  { loader :'css-loader'}]
+          },
+          {
+              test:/\.html$/,
+              use:['html-loader']
+          },
+          {
+              test:/\.(png|jpg|gif|ttf|jpg)$/,
+              use:['url-loader']
+          }
+      ],
+    },
+    devtool: "source-map",
+    output:{
+        filename:'[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean:true,
+    },
+}
